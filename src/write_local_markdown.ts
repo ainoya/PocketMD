20f4f711cf462e01fbfde1060b4a4417c787c079
Naoki Ainoya <ainonic@gmail.com>
2024-06-19 10:47:31 +0900

    feat: Format markdown body in saveArticleToMarkdown function

--- src/write_local_markdown.ts
+++ src/write_local_markdown.ts
@@ -6,6 +6,7 @@ import { db } from "./db";
 import { articlesTable as articles } from "./schema";
 import fs from "fs";
 import path from "path";
+import crypto from "crypto";
 
 const CLIP_DIR = process.env.CLIP_DIR;
 
@@ -32,6 +33,72 @@ async function main() {
 
 main().catch(console.error);
 
+const markdownBodyFormatOptions = {
+  removeImageTags: process.env.MARKDOWN_FORMAT_REMOVE_IMAGE_TAGS === "true",
+  replaceBase64ImageToFile:
+    process.env.MARKDOWN_FORMAT_REPLACE_BASE64_IMAGE_TO_FILE === "true",
+};
+
+const formatMarkdownBody = (markdownBody: string): string => {
+  const formatRuleFunctions: Array<((body: string) => string) | undefined> = [
+    markdownBodyFormatOptions.removeImageTags
+      ? (body: string) => body.replace(/!\[.*\]\(.*\)/g, "")
+      : undefined,
+    markdownBodyFormatOptions.replaceBase64ImageToFile
+      ? (body: string) => {
+          // replace base64 image to file
+          // convert base64 image to file
+          // get image directory path from env
+          // by default, save to the `${CLIP_DIR}/images` directory
+          const imageDir = process.env.MARKDOWN_FORMAT_IMAGE_DIR || `./images`;
+          if (!process.env.CLIP_DIR) {
+            throw new Error("CLIP_DIR is not set");
+          }
+          if (!fs.existsSync(path.join(process.env.CLIP_DIR, imageDir))) {
+            fs.mkdirSync(path.join(process.env.CLIP_DIR, imageDir));
+          }
+          const imageDirAbsolute = path.join(process.env.CLIP_DIR, imageDir);
+          const base64ImageRegex =
+            /!\[.*\]\(data:image\/(.*);base64,([^"]*)\)/g;
+          const base64ImageMatch = body.match(base64ImageRegex);
+
+          // convert base64 image to file
+          if (base64ImageMatch) {
+            base64ImageMatch.forEach((base64Image) => {
+              const base64ImageRegex =
+                /!\[.*\]\(data:image\/(.*);base64,([^"]*)\)/;
+              const match = base64Image.match(base64ImageRegex);
+              if (match) {
+                const [, ext, base64] = match;
+                // filename is the hash of the sha256 of the base64 string, using crypto
+                const fileName = `${crypto
+                  .createHash("sha256")
+                  .update(base64)
+                  .digest("hex")}.${ext.replace("+xml", "")}`;
+                const filePath = path.join(imageDirAbsolute, fileName);
+                fs.writeFileSync(filePath, Buffer.from(base64, "base64"));
+                const filePathRelative = path.join(imageDir, fileName);
+                body = body.replace(
+                  base64Image,
+                  `![image](${filePathRelative})`
+                );
+              }
+            });
+          }
+
+          return body;
+        }
+      : undefined,
+  ];
+
+  return formatRuleFunctions.reduce((body, rule) => {
+    if (rule) {
+      return rule(body);
+    }
+    return body;
+  }, markdownBody);
+};
+
 async function saveArticleToMarkdown(article: {
   id: string;
   title: string | null;
@@ -52,17 +119,19 @@ async function saveArticleToMarkdown(article: {
   const fileName = `${article.title.replace(/[/\\?%*:|"<>]/g, "_")}.md`;
   const filePath = path.join(CLIP_DIR, fileName);
 
-  if (fs.existsSync(filePath)) {
-    console.log(`File ${filePath} exists, skipping saving`);
-    return;
-  }
+  // if (fs.existsSync(filePath)) {
+  //   console.log(`File ${filePath} exists, skipping saving`);
+  //   return;
+  // }
 
-  const markdownBody = article.markdown;
+  const markdownBody = article.markdown
+    ? formatMarkdownBody(article.markdown)
+    : undefined;
 
   const content = `---
-title: "${article.title}"
+title: "${sanitizeFrontmatterString(article.title)}"
 url: ${article.url}
-description: "${article.excerpt}"
+description: "${sanitizeFrontmatterString(article.excerpt ?? "")}"
 ---
 
 ## Summary
@@ -78,3 +147,7 @@ ${markdownBody ?? "no content"}
 
   console.log(`Saved article ${article.title} to ${filePath}`);
 }
+
+const sanitizeFrontmatterString = (str: string) => {
+  return str.replace(/"/g, '\\"');
+};
