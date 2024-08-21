9c5e4149cf6cb9b454f7dbaf1d41b2d8bfbf6cf3
Naoki Ainoya <ainonic@gmail.com>
2024-08-21 09:01:14 +0900

    try-catch

--- src/index.ts
+++ src/index.ts
@@ -28,30 +28,35 @@ async function saveArticlesToSQLite(newArticles: PocketItemList) {
   // Insert articles into the database
   for (const articleId in newArticles) {
     const article = newArticles[articleId];
-    console.log(
-      `Saving url ${article.resolved_url},article: ${
-        article.resolved_title
-      }, timestamp: ${format(
-        new Date(parseInt(article.time_added) * 1000),
-        "yyyy-MM-dd HH:mm:ss"
-      )}`
-    );
-    // upsert into the articles table
-    await db
-      .insert(articles)
-      .values({
-        id: article.item_id,
-        title: article.resolved_title ?? article.given_title,
-        url: article.resolved_url ?? article.given_url,
-        excerpt: article.excerpt,
-        raw_data: article,
-        // convert time_added (epoch seconds) to date
-        time_added: new Date(parseInt(article.time_added) * 1000),
-      })
-      .onConflictDoNothing({
-        target: articles.id,
-      })
-      .execute();
+    try {
+      console.log(
+        `Saving url ${article.resolved_url},article: ${
+          article.resolved_title
+        }, timestamp: ${format(
+          new Date(parseInt(article.time_added) * 1000),
+          "yyyy-MM-dd HH:mm:ss"
+        )}`
+      );
+      // upsert into the articles table
+      await db
+        .insert(articles)
+        .values({
+          id: article.item_id,
+          title: article.resolved_title ?? article.given_title,
+          url: article.resolved_url ?? article.given_url,
+          excerpt: article.excerpt,
+          raw_data: article,
+          // convert time_added (epoch seconds) to date
+          time_added: new Date(parseInt(article.time_added) * 1000),
+        })
+        .onConflictDoNothing({
+          target: articles.id,
+        })
+        .execute();
+    } catch (error) {
+      console.error(`Error saving article: ${article.resolved_url}, ${error}`);
+      console.error(error);
+    }
   }
 }
 
